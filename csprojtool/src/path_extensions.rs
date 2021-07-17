a91869218cd566eca1b2c65639efa48cedaa1e37
Mick van Gelderen <mickvangelderen@gmail.com>
2021-07-17 09:47:23 +0200

    Add tests to PathExt::simplify

--- csprojtool/src/path_extensions.rs
+++ csprojtool/src/path_extensions.rs
@@ -25,23 +25,68 @@ pub trait PathExt {
 
 impl PathExt for Path {
     fn simplify(&self) -> PathBuf {
-        let mut stack = Vec::new();
+        let mut result = PathBuf::with_capacity(self.as_os_str().len());
+
         for current in self.components() {
             match current {
                 Component::CurDir => {}
                 Component::Prefix(_) | Component::RootDir | Component::Normal(_) => {
-                    stack.push(current)
+                    result.push(current);
                 }
                 Component::ParentDir => {
-                    let should_pop = matches!(stack.last(), Some(Component::Normal(_)));
-                    if should_pop {
-                        stack.pop();
+                    if matches!(result.components().last(), Some(Component::Normal(_))) {
+                        result.pop();
                     } else {
-                        stack.push(current);
+                        result.push(current);
                     }
                 }
             }
         }
-        return stack.iter().collect();
+
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn pathbuf_only_retains_last_absolute_path() {
+        assert_eq!(
+            Path::new(r"\\?\D:\Data\files.txt"),
+            [r"\\?\C:\Users\Rust\README.md", r"\\?\D:\Data\files.txt"]
+                .iter()
+                .collect::<PathBuf>()
+        );
+    }
+
+    #[test]
+    fn simplify_works() {
+        assert_eq!(
+            Path::new(r"\\?\D:\Data\files.txt"),
+            Path::new(r"\\?\D:\Data\Nested\..\files.txt").simplify(),
+        );
+
+        assert_eq!(
+            Path::new(r"\\?\D:\files.txt"),
+            Path::new(r"\\?\D:\Data\Nested\..\..\files.txt").simplify(),
+        );
+    }
+
+    #[test]
+    fn simplify_drops_curdir() {
+        assert_eq!(
+            Path::new(r"\\?\D:\files.txt"),
+            Path::new(r"\\?\D:\.\files.txt").simplify(),
+        );
+    }
+
+    #[test]
+    fn simplify_retains_parent_at_root() {
+        assert_eq!(
+            Path::new(r"\\?\D:\..\files.txt"),
+            Path::new(r"\\?\D:\..\files.txt").simplify(),
+        );
     }
 }
